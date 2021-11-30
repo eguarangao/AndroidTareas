44fcf240018fbbf7cebdffd9da30ae25177cb13c
eguarangao <69706207+eguarangao@users.noreply.github.com>
2021-11-30 12:27:29 -0500

    Culminacion
    Falta arreglar la fecha y el envio de la misma asi como su telefono

--- Practica1/app/src/main/java/com/example/practica1/Activity2.java
+++ Practica1/app/src/main/java/com/example/practica1/Activity2.java
@@ -3,6 +3,7 @@ package com.example.practica1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -10,5 +11,24 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        //Localizar los controles
+        TextView mensaje;
+        mensaje = findViewById(R.id.txtRMensaje);
+
+        //Recuperamos la información pasada en el intent
+        Bundle bundel = this.getIntent().getExtras();
+
+        //Construimos el mensaje a mostrar
+        String cadena;
+
+        if (bundel.getString("genero").equals("Masculino"))
+            cadena = "Hola, bienvenido " + bundel.getString("nombre");
+        else
+            cadena = "Hola, bienvenida " + bundel.getString("nombre");
+
+        cadena += " su fecha de nacimiento es " + bundel.getString("f_nacimiento")
+                + ", y su número de télefono es: " + bundel.getString("telefono") + " y lo registramos para contactarnos en un futuro próximo. " +
+                "\n\nGracias por su atención y confiar en nosotros.";
+        mensaje.setText(cadena);
     }
 }
--- Practica1/app/src/main/java/com/example/practica1/MainActivity.java
+++ Practica1/app/src/main/java/com/example/practica1/MainActivity.java
@@ -1,19 +1,73 @@
 package com.example.practica1;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText name, date, phone;
+    int dia,mes,anio;
+    RadioButton women,men;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.edNombre);
+        date = findViewById(R.id.edFechaN);
+        phone = findViewById(R.id.edTelefono);
+
+        women = findViewById(R.id.rbtFemenino);
+        men = findViewById(R.id.rbtMasculino);
     }
-    public void btnGet(View view){
-        Intent intent = new Intent();
+    public void Get(){
+        Intent intent = new Intent(MainActivity.this,Activity2.class);
+        Bundle bundel = new Bundle();
+        bundel.putString("nombre",name.getText().toString());
+        bundel.putString("f_nacimiento",date.getText().toString());
+        bundel.putString("phone",phone.getText().toString());
+        if(women.isChecked()){
+            bundel.putString("genero",women.getText().toString());
+        }
+        if(men.isChecked()){
+            bundel.putString("genero",men.getText().toString());
+        }
+        intent.putExtras(bundel);
+        startActivity(intent);
+
+    }
+
+    public void clickBtn(View view){
+        if(name.getText().toString().equals("") || date.getText().toString().equals("") || (!women.isChecked() && !men.isChecked()) || phone.getText().toString().equals(""))
+            Toast.makeText(getApplicationContext(), "Campos obligatorios", Toast.LENGTH_SHORT).show();
+        else if(phone.getText().length() != 10)
+            Toast.makeText(getApplicationContext(), "La cantidad de dígitos que debe colocar en su télefono es 10", Toast.LENGTH_SHORT).show();
+        else
+            Get();
+    }
+    public void vistaCalendar(View view)
+    {
+        final Calendar calendar = Calendar.getInstance();
+
+        dia = calendar.get(Calendar.DAY_OF_MONTH);
+        mes = calendar.get(Calendar.MONTH);
+        anio = calendar.get(Calendar.YEAR);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, (view1, year, month, dayOfMonth) -> {
+            String fecha = dayOfMonth + "/" + (month + 1) + "/" + year;
+            date.setText(fecha);
+        }, anio, mes, dia);
+        datePickerDialog.show();
+
     }
 }
